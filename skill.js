2da2e0d852ad4474bcc8a26a7238c96d947042ef
Jedião Andrade <ericdemlo244@gmail.com>
2022-09-27 10:41:57 +0100

    commit inicial
    Commit da hospedagem do site

---
+++ skill.js
@@ -0,0 +1,25 @@
+function html(){
+    let areatexto=document.getElementById("texto");
+
+    areatexto.innerHTML=`<h3>HTML<h3>
+    HTML é uma linguagem de marcação que surgiu  em 1976 para a criação de conteúdos suportados em navegadores`
+}
+function css() {
+  let areatexto = document.getElementById("texto");
+
+  areatexto.innerHTML = `<h3>CSS<h3>
+    CSS é uma linguagem de marcação que surgiu  em 1976 para a criação de conteúdos suportados em navegadores`;
+}
+function js() {
+  let areatexto = document.getElementById("texto");
+
+  areatexto.innerHTML = `<h3>JS<h3>
+    Java Script é uma linguagem de marcação que surgiu  em 1976 para a criação de conteúdos suportados em navegadores`;
+}
+function php() {
+  let areatexto = document.getElementById("texto");
+
+  areatexto.innerHTML = `<h3>PHP<h3>
+    PHP é uma linguagem de marcação que surgiu  em 1976 para a criação de conteúdos suportados em navegadores`;
+}
+
